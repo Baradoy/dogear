1e1efbd4f69e9081e89a8d2dd9c9343ea270d15e
Graham Baradoy <graham@baradoy.ca>
2022-05-25 17:36:05 -0700

    Add CI

--- mix.exs
+++ mix.exs
@@ -47,7 +47,9 @@ defmodule Dogear.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1.0", only: [:dev], runtime: false}
     ]
   end
 
