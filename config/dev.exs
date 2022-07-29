eccc45308ad1666ee97e51b0417a50341027976a
Graham Baradoy <graham@baradoy.ca>
2022-07-29 13:16:10 -0700

    Add setting upload path in config

--- config/dev.exs
+++ config/dev.exs
@@ -69,3 +69,5 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :dogear, uploads_path: File.cwd!() <> "/priv/uploads/"
